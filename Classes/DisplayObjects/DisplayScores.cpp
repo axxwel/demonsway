bd24a74c09d2dbccad847c25b2acc8455dbd8c55
alexandre_gimeno <axxwel@gmail.com>
2020-10-29 07:52:15 +0100

    add score file

---
+++ Classes/DisplayObjects/DisplayScores.cpp
@@ -0,0 +1,8 @@
+//
+//  DisplayScores.cpp
+//  cocos2d
+//
+//  Created by alexandre gimeno on 28/10/2020.
+//
+
+#include "DisplayScores.hpp"
---
+++ Classes/DisplayObjects/DisplayScores.hpp
@@ -0,0 +1,13 @@
+//
+//  DisplayScores.hpp
+//  cocos2d
+//
+//  Created by alexandre gimeno on 28/10/2020.
+//
+
+#ifndef DisplayScores_hpp
+#define DisplayScores_hpp
+
+#include <stdio.h>
+
+#endif /* DisplayScores_hpp */
